a86f48f88a01a298a557db6effc47b2a73d861bd
Arnas Mitkevičius <45790150+AronMitk@users.noreply.github.com>
2020-11-23 20:09:13 +0200

    Add files via upload

---
+++ SpaceInvadersTest/EnemyBulletTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Space_Invaders.Code;
+using System;
+
+namespace SpaceInvadersTest
+{
+    [TestClass]
+    public class EnemyBulletTests
+    {
+        [TestMethod]
+        public void MoveForwardTest()
+        {
+            int expectedX = 0;
+            int expectedY = 10;
+
+            EnemyBullet enemyBullet = new EnemyBullet
+            {
+                X = 0,
+                Y = 0
+            };
+
+            enemyBullet.MoveForward();
+
+            int actualX = enemyBullet.X;
+            int actualY = enemyBullet.Y;
+
+            Assert.AreEqual(expectedX, actualX, "Bullet X is not correct");
+            Assert.AreEqual(expectedY, actualY, "Bullet Y is not correct");
+        }
+    }
+}
